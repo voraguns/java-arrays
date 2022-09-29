cbb1316db01cc990a71de0845c9065ff149e14d4
Sharinogan <voragun2543@gmail.com>
2022-09-29 18:10:33 +0700

    functional programming forEach() function

--- code/Start.java
+++ code/Start.java
@@ -10,10 +10,23 @@ class Start {
         list.add(new Player("Frank L", 8));
         list.add(new Player("David J", 1));
         
+        list.stream().forEach(p -> System.out.println(p.name()));
+        
+        // Write code to find the total of number of each player
+//        var w = list.stream().reduce(0, (result, (Object)p) -> {
+//            System.out.println((Player)p);
+//            return result + ((Player)p).number();
+//        });
+//        System.out.println(w);
+        
         var a = new LinkedList<Integer>();
         for (int i = 0; i < 10; i++) a.add(i);
         Integer total = a.stream().reduce(0, (result, value) -> result + value);
         System.out.println(total);
+        
+        // from the variable a, write chain code to print only even value
+        a.stream().filter(x -> x % 2 == 0).forEach(x -> System.out.println(x));
+        
     }
 }
 
