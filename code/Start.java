0a9bd410906c45d6cd78e07de4762629e7c29ed7
Sharinogan <voragun2543@gmail.com>
2022-09-29 17:38:22 +0700

    Fillter

--- code/Start.java
+++ code/Start.java
@@ -1,33 +1,34 @@
-
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.LinkedList;
 
 class Start {
     public static void main(String[] data) {
-        Player[] team = { new Player("David B", 7),
-                           new Player("Michael O", 10),
-                           new Player("Frank L", 8),
-                            new Player("David J", 1)};
-        Arrays.sort(team, (p, q) -> p.name().compareTo(q.name()));
-        for(Player p : team) {
-            System.out.println( p.name() + " " + p.number());
-        }
-    }
-}
-
-class Show {
-    void print(int n) {
-        if (n == 0) return;
-        print(n-1);
-        System.out.println(n);
-    }
-}
-
-class PlayerComparator implements Comparator {
-    public int compare(Object a, Object b) {
-        Player p = (Player)a;
-        Player q = (Player)b;
-        return p.number() - q.number();
+        
+        var list = new LinkedList<Player>();
+        
+        list.add(new Player("David B", 7));
+        list.add(new Player("Michael O", 10));
+        list.add(new Player("Frank L", 8));
+        list.add(new Player("David J", 1));
+        
+        // ถามทีละตัว
+        var result = list.stream()
+                .filter(x -> x.number() >= 8)
+                .toList();
+        System.out.println(list);
+        System.out.println(result);
+        
+        var a = new LinkedList<Integer>();
+        for (int i = 0; i < 40000000; i++) a.add(i);
+        long start = System.currentTimeMillis();
+        long stop = System.currentTimeMillis();
+        // write code to fillter only value that can divide by 785
+        
+        // ทำงานตาม CPU
+//        var b = a.parallelStream()
+//                .filter(x -> x % 785 == 0)
+//                .toList();
+//        System.out.println(b);
+        System.out.println(start - stop);
     }
 }
 
